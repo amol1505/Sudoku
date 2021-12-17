c53264322f1baa8c623faac90f9a55128a1e5360
amol1505 <66383251+amol1505@users.noreply.github.com>
2021-12-17 19:34:20 +0000

    Delete Main.hs

--- Main.hs
+++
@@ -1,9 +0,0 @@
-module Main 
-    where
-import Sudoku
-
-main :: IO ()
-main = do s <- readAndSolve "../puzzles/easy10.sud"
-          case s of
-            Nothing  -> putStrLn "Solving puzzle failed"
-            (Just s) -> printPuzzle s
